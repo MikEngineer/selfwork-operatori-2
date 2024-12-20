d18d764c5ee23716fcb6b34799699c348f62f427
Michelangelo Ruisi <mruisi96@gmail.com>
2024-12-20 11:07:53 +0100

    terminato selfwork operatori 2

---
+++ main.js
@@ -0,0 +1,13 @@
+let annoCorrente = 2024;
+let annoNascita = 1996;
+
+let età = annoCorrente - annoNascita;
+
+console.log(età);
+
+let centenario = 100 - età;
+
+console.log(centenario);
+
+console.log(`Hai ${età} anni e ti mancano ${centenario} anni per compierne 100`);
+
